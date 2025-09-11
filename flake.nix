9fc63b02e08174106a709d83dcd14d55331bd4f8
Harshal Sawant <harshalsawantdev@gmail.com>
2025-09-12 00:42:01 +0530

    feat: migrate to flake and remove outdated configuration files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "PHP flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forAll = f: builtins.listToAttrs (map (system: { name = system; value = f system; }) systems);
+  in {
+    devShells = forAll (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            php
+            just
+            nodejs
+            bun
+          ];
+        };
+      });
+
+    formatter = forAll (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in pkgs.nixpkgs-fmt
+    );
+  };
+}
---
+++ justfile
@@ -0,0 +1,7 @@
+# Default recipe
+default:
+    @just --list
+
+# Run as localhost
+run:
+    php -S localhost:8000
